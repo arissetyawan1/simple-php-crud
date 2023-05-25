534dab101d4b8f73f6aef465ae547a1e84e09cb5
Aris Setyawan <aris.setyawan354313@gmail.com>
2023-05-25 23:15:53 +0700

    fix : simple crud

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
 		if (count(array($record)) == 1 ) {
 			$n = mysqli_fetch_array($record);
 			$nama = $n['nama'];
-			$kota = $n['kota'];
+			$no_meja = $n['no_meja'];
+			$detail_pesanan = $n['detail_pesanan'];
 		}
 	}
 ?>
@@ -18,54 +19,75 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Learn PHP7 | Simple-CRUD single page</title>
-	<style>
-		table, th, td {
-			border: solid 1px #000;
-			text-align: left;
-		}
-	</style>
+	<title>UAS Crud Restaurant</title>
+	<link rel="stylesheet" href="style.css">
 </head>
 <body>
-
+	<!-- header -->
+	<div class="header">
+		<span>
+			UAS Crud Restaurant
+		</span>
+	</div>
 	<!-- Alert jika CRUD data -->
 	<?php if (isset($_SESSION['alert'])) : ?>
-	<h2>
-		<?php
-			echo $_SESSION['alert'];
-			unset($_SESSION['alert']);
-		?>
-	</h2>
+	<label>
+		<input type="checkbox" class="alertCheckbox" autocomplete="off" />
+		<div class="alert success">
+			<span class="alertClose">X</span>
+			<span class="alertText">
+				<?php
+					echo $_SESSION['alert'];
+					unset($_SESSION['alert']);
+				?>
+				<br class="clear" />
+			</span>
+		</div>
+	</label>
 	<?php endif ?>
 
-	<!-- Form CRUD data -->
-	<form action="server.php" method="POST">
-		<input type="hidden" name="id" value="<?php echo $id; ?>">
-		<input type="text" name="nama" placeholder="Masukan Nama" value="<?php echo $nama; ?>">
-		<input type="text" name="kota" placeholder="Masukan Kota" value="<?php echo $kota; ?>">
-		<?php if ($update == true): ?>
-		<input type="submit" name="update" value="Update">
-		<?php else: ?>
-		<input type="submit" name="save" value="Save">
-		<?php endif ?>
-	</form>
-
-	<!-- Table data -->
-	<table>
+	<div class="content">
+		<h3>
+			Pesan Di sini
+		</h3>
+		<!-- Form CRUD data -->
+		<form action="server.php" method="POST" class="form">
+				<div class="form-input">
+					<input type="hidden" name="id" value="<?php echo $id; ?>">
+					<input type="text" name="nama" placeholder="Masukan Nama" value="<?php echo $nama; ?>" class="input">
+					<input type="text" name="no_meja" placeholder="Masukan No Meja" value="<?php echo $no_meja; ?>" class="input">
+					<textarea type="textarea" name="detail_pesanan" placeholder="Masukan Detail Pesanan" value="<?php echo $detail_pesanan; ?>" class="input" rows="4" cols="50"></textarea>
+					<?php if ($update == true): ?>
+					<input type="submit" name="update" value="Update" class="button">
+					<?php else: ?>
+					<input type="submit" name="save" value="Save" class="button">
+					<?php endif ?>
+				</div>
+			</form>
+		<!-- Table data -->
+		<table class="styled-table">
 		<thead>
-			<th>Nama</th>
-			<th>Kota</th>
-			<th colspan="2">Setting</th>
-		</thead>
-		<?php while ($row = mysqli_fetch_array($result)) { ?>
-		<tr>
-			<td><?php echo $row['nama']; ?></td>
-			<td><?php echo $row['kota']; ?></td>
-			<td><a href="index.php?edit=<?php echo $row['id'] ?>">Edit</a></td>
-			<td><a href="server.php?del=<?php echo $row['id'] ?>">Delete</a></td>
-		</tr>
-		<?php } ?>
-	</table>
+			<thead>
+				<tr>
+					<th>Nama</th>
+					<th>No Meja</th>
+					<th>Detail Pesanan</th>
+					<th colspan="2">Setting</th>
+				</tr>
+			</thead>
+			<?php while ($row = mysqli_fetch_array($result)) { ?>
+			<tbody>
+				<tr>
+					<td><?php echo $row['nama']; ?></td>
+					<td><?php echo $row['no_meja']; ?></td>
+					<td style="width:50%"><?php echo $row['detail_pesanan']; ?></td>
+					<td><a href="index.php?edit=<?php echo $row['id'] ?>" class="edit">Edit</a></td>
+					<td><a href="server.php?del=<?php echo $row['id'] ?>" class="delete">Delete</a></td>
+				</tr>
+			</tbody>
+			<?php } ?>
+		</table>
+	</div>
 
 </body>
 </html>
--- server.php
+++ server.php
@@ -5,16 +5,18 @@
 
     // Initialize Variables
     $nama = "";
-    $kota = "";
+    $no_meja = "";
+    $detail_pesanan = "";
     $id = 0;
     $update = false;
 
     // POST Add new data
     if (isset($_POST['save'])) {
         $nama = $_POST['nama'];
-        $kota = $_POST['kota'];
+        $no_meja = $_POST['no_meja'];
+        $detail_pesanan = $_POST['detail_pesanan'];
 
-        mysqli_query($db, "INSERT INTO user (nama, kota) VALUES ('$nama', '$kota')");
+        mysqli_query($db, "INSERT INTO user (nama, no_meja, detail_pesanan) VALUES ('$nama', '$no_meja', '$detail_pesanan')");
         $_SESSION['alert'] = "Data saved!";
         header("location: index.php");
     }
@@ -23,9 +25,10 @@
     if (isset($_POST['update'])) {
         $id = $_POST['id'];
         $nama = $_POST['nama'];
-        $kota = $_POST['kota'];
+        $no_meja = $_POST['no_meja'];
+        $detail_pesanan = $_POST['detail_pesanan'];
 
-        mysqli_query($db, "UPDATE user SET nama='$nama', kota='$kota' WHERE id=$id");
+        mysqli_query($db, "UPDATE user SET nama='$nama', no_meja='$no_meja', detail_pesanan='$detail_pesanan' WHERE id=$id");
         $_SESSION['alert'] = "Data updated!";
         header("location: index.php");
     }
